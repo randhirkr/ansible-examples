b3e0a87ec3e7536a09fa28f2cc1ea2c0883c9ed2
randhirkr <cusatrandhir@gmail.com>
2021-02-14 10:28:35 -0800

    added files for users specific installation

---
+++ files/user-specific-installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+curl -o install.sh https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh
+bash install.sh
+source ~/.bashrc
+nvm install node
+
+chmod u=rwx,g=rx,o=rx ~
+
+curl -o install.sh https://raw.githubusercontent.com/c9/install/master/install.sh
+bash install.sh
+
